763f10adeb2f906fe89e87752c9430ed323c4ba3
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-31 16:45:09 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div className="flex">Hi!</div>
+      <div className="bg-violet-200 text-violet-950 flex flex-col p-4 min-w-[300px] min-h-[300px]">
+        <p className="text-lg font-medium">
+          Lorem! Ipsum is simply dummy text of the printing and typesetting
+        </p>
+      </div>
     </>
   );
 };
